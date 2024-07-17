30b5dcb0c1121b7b07f2ebbc6a60aeee24fbf0e2
vkrukhmalev <Krukhmalev02@gmail.com>
2024-07-16 23:17:50 -0400

    Added TTF_Text loading to the asset database

--- src/asset_database.cpp
+++ src/asset_database.cpp
@@ -1,13 +1,14 @@
 #include "asset_database.h"
 
 #include "logger.h"
+#include "unique_ptrs_destructors.h"
 
 namespace pong {
 	AssetDatabase::AssetDatabase(const SDL_Renderer* renderer) :
 		renderer_(renderer)
 	{}
 
-	const Texture* AssetDatabase::GetTexture(std::string name) const {
+	const Texture* AssetDatabase::GetTexture(const std::string& name) const {
 		if (textures_.find(name) != textures_.end()) {
 			return &textures_.at(name);
 		}
@@ -20,6 +21,19 @@ namespace pong {
 		return &textures_.at(name);
 	}
 
+	const TTF_Font* AssetDatabase::GetFont(const std::string& name, int size) const {
+		if (fonts_.find(name) != fonts_.end()) {
+			return fonts_.at(name).get();
+		}
+
+		if (!LoadFont(name, size, "resources/Fonts/" + name)) {
+			logger::LogAndShowError(SDL_GetError());
+			return nullptr;
+		}
+
+		return fonts_.at(name).get();
+	}
+
 	bool AssetDatabase::LoadTexture(const std::string& name, const std::string& path) const {
 		Texture tex = Texture(renderer_);
 
@@ -27,11 +41,22 @@ namespace pong {
 			logger::LogAndShowError(SDL_GetError());
 			return false;
 		}
-		auto gf = std::make_pair(name, tex);
 
 
 		
 		textures_.insert(std::make_pair(name, tex));
+		return true;
+	}
+
+	bool AssetDatabase::LoadFont(const std::string& name, int size, const std::string& path) const {
+		TTF_Font* font = TTF_OpenFont(path.c_str(), size);
+
+		if (!font) {
+			logger::LogAndShowError(TTF_GetError());
+			return false;
+		}
+
+		fonts_.emplace(name, std::unique_ptr<TTF_Font, TtfFontDestructor>(font));
 		return true;
 	}
 }
--- src/asset_database.h
+++ src/asset_database.h
@@ -1,9 +1,11 @@
 #ifndef ASSET_DATABASE_H
 #define ASSET_DATABASE_H
 #include <map>
+#include <SDL_ttf.h>
 #include <string>
 
 #include "texture.h"
+#include "unique_ptrs_destructors.h"
 
 namespace pong {
 
@@ -21,16 +23,21 @@ namespace pong {
 
 
 		//Returns a pointer to a texture. Loads it if it wasn't loaded before
-		const Texture* GetTexture(std::string name) const;
+		const Texture* GetTexture(const std::string& name) const;
+		//Returns a pointer to a font. Loads it if it wasn't loaded before
+		const TTF_Font* GetFont(const std::string& name, int size) const;
 
 	private:
 		//To load textures
 		const SDL_Renderer* renderer_;
 
-		//Loads texture located at path relative to the resources folder
+		//Loads the texture from disk
 		bool LoadTexture(const std::string& name, const std::string& path) const;
+		//Loads the font from disk
+		bool LoadFont(const std::string& name, int size, const std::string& path) const;
 
 		mutable std::map<std::string, Texture> textures_;
+		mutable std::map<std::string, std::unique_ptr<TTF_Font, TtfFontDestructor>> fonts_;
 
 	};
 }
